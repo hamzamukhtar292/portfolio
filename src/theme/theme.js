f97ae4ce4fa4062486df4eb14fdff55e02fa0baa
Hamza Mukhtar <hamzamukhtar292@gmail.com>
2023-07-30 01:58:31 +0500

    changes

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,25 +1,25 @@
 /* eslint-disable */
-import { contactsGreen } from './images'
-
+import { contactsGreen } from "./images";
 
 export const theLightTheme = {
-    type: 'light',
-    primary: '#1D9BF0',
-    quaternary: '#EFF3F4',
-    quaternaryLight: '#F7F9F9',
-    secondary: '#ffffff',
-    tertiary: '#0f1419',
-    buttonColor: '#0F1419',
-    contactsimg: contactsGreen
-}
+  type: "light",
+  primary: "#BADFE7",
+  quaternary: "#EFF3F4",
+  quaternaryLight: "#F7F9F9",
+  secondary: "#ffffff",
+  tertiary: "#6FB3B8",
+  buttonColor: "#0F1419",
+  contactsimg: contactsGreen,
+};
 
 export const theDarkTheme = {
-    type: 'dark',
-    primary: '#1D9BF0',
-    secondary: '#0F0C41',
-    quaternary: '#0C0B33',
-    quaternaryLight: '#8b98a5',
-    tertiary: '#EFF3F4',
-    buttonColor: '#8B98A5',
-    contactsimg: contactsGreen
-}
+  type: "dark",
+  iconbg: "#C2EDCE",
+  primary: "#F6F6F2",
+  secondary: "#388087",
+  quaternary: "#6FB3B8",
+  quaternaryLight: "#8b98a5",
+  tertiary: "#EFF3F4",
+  buttonColor: "#8B98A5",
+  contactsimg: contactsGreen,
+};
